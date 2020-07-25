55545fc7116491df58042742525545ea7be80892
EliasMoura <d.eliasmoura2@gmail.com>
2020-07-25 15:04:27 -0300

    Modificando los proyectos del porfolio

--- index.php
+++ index.php
@@ -1,11 +1,78 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Moura</title>
+    <title>Portfolio Moura Elias</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100&display=swap" rel="stylesheet">
+    <link rel="stylesheet" 
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" 
+            integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" 
+            crossorigin="anonymous" />
+    <link rel="stylesheet" href="main.css">
 </head>
 <body>
-    <h1>Bienvenido a mi portfolio</h1>
+    <header>
+        <h1 id="greet"><strong>Bienvenido!</strong></h1>
+        <p id="presentation" class="user-select-none">
+            Me llamo Moura Elias, soy desarrollador javascript junior y estudiante de ingenieria 
+            en informatica.<br>
+            Actualmente trabajo en la Escuela de Robotica de la 
+            Provincia de Misiones, Argentina. <br>
+        </p>
+    </header>
+    <!-- <nav>
+        <ul>
+            <li>Home</li>
+                form-group row
+        </ul>
+    </nav> -->
+
+        
+    <div class="container shadow-lg p-3 mb-5 rounded">
+        <div class="row justify-content-center">
+            <h1><strong>Proyectos</strong></h1>
+        </div>
+        <hr class="my-3">
+        <div class="row">
+            <div class="col-sm">
+                <div class="card" style="width: 20rem;">
+                    <img src="./assets/images/HTML-CSS-JS.png" class="card-img-top" style="" alt="...">
+                    <div class="card-body">
+
+                        <h5 class="card-title text-body"><strong>Todo List</strong></h5>
+                        <p class="card-text text-body"><strong>proyecto Todo list con HTML, CSS y Javascript</strong></p>
+                        <a href="https://todo-basic-js-localstorage.herokuapp.com/" class="btn btn-primary btn-lg btn-block text-body"><strong>VER</strong></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="card" style="width: 20rem;">
+                    <img src="./assets/images/HTML-CSS-JS.png" class="card-img-top" style="" alt="...">
+                    <div class="card-body">
+
+                        <h5 class="card-title text-body"><strong>Todo List</strong></h5>
+                        <p class="card-text text-body"><strong>proyecto Todo list con HTML, CSS y Javascript</strong></p>
+                        <a href="https://todo-basic-js-localstorage.herokuapp.com/" class="btn btn-primary btn-lg btn-block text-body"><strong>VER</strong></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="card" style="width: 20rem;">
+                    <img src="./assets/images/HTML-CSS-JS.png" class="card-img-top" style="" alt="...">
+                    <div class="card-body">
+
+                        <h5 class="card-title text-body"><strong>Todo List</strong></h5>
+                        <p class="card-text text-body"><strong>proyecto Todo list con HTML, CSS y Javascript</strong></p>
+                        <a href="https://todo-basic-js-localstorage.herokuapp.com/" class="btn btn-primary btn-lg btn-block text-body"><strong>VER</strong></a>
+                    </div>
+                </div>
+            </div>          
+        </div>
+    </div>
+
+
 </body>
 </html>
